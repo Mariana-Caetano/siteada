8326dcaf0bb5415307028c166b136b9c36884614
Mariana Caetano Pereira <nanah.caetano@gmail.com>
2020-09-20 14:19:28 +0000

    Element "celular"

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 document.getElementById("botaoQuero").addEventListener("click", validaFormulario)
 
 function validaFormulario(){
-  if (document.getElementById("nome").value != "" && document.getElementById("email").value != "") {
+  if (document.getElementById("nome").value != "" && document.getElementById("email").value != "" && document.getElementById("celular").value != "") {
   alert("Uhul! Você receberá as novidades por email.")
   }else{
-    alert("Ops!Preencha os campos nome e email.") 
+    alert("Ops!Preencha os campos nome, email e celular.") 
   }  
 }
 
