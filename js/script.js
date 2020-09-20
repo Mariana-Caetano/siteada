130430954cf86dcadf823223179e83ef107b04e3
Mariana Caetano <nanah.caetano@gmail.com>
2020-09-20 01:55:19 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+dequemviráoevento.addEventListener("evento", oquedevofazer)
+
+if (document.getElementById("nome").value != "" && document.getElementById("email").value != "") {
+  alert("Uhul! Você receberá as novidades por email.")
+}else{
+  alert("Ops!Preencha os campos nome e email.") 
+}  
